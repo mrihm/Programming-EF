a9fa1c22cf04d6133d5dc3ba84813430d3b446d2
Mischa Rihm <mischa.rihm@gmail.com>
2015-01-22 15:59:54 -0600

    Commit the module 04 project

---
+++ src/AppDev/AdventureWorksSample/AdventureWorks.EF/AWLTEntities.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'G:\Coding\Programming EF\src\AppDev\AdventureWorksSample\AdventureWorks.EF\AWLTEntities.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ src/AppDev/AdventureWorksSample/AdventureWorks.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventureWorks.EF;
+
+namespace AdventureWorks.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var awlt = new AWLTEntities();
+            var prods = awlt.Products.Where(x => x.Name.StartsWith("F")).OrderBy(x => x.Name);
+
+            Console.WriteLine("Selected Products:");
+            Console.WriteLine("------------------");
+
+            foreach(var prod in prods)
+                Console.WriteLine("{0}, ${1}", prod.Name, prod.ListPrice.ToString("#,##0.00"));
+
+            Console.WriteLine("\nPress <enter> to continue...");
+            Console.ReadLine();
+        }
+    }
+}
