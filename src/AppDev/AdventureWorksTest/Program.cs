99bc5f99ddd282952ec2631ac540c6cb9c1cc500
Mischa Rihm <mischa.rihm@gmail.com>
2015-01-22 15:44:55 -0600

    Revert "Revert "Commit the completed module 4 project""
    This reverts commit 511681828cbb9f5ca3897de33eca85889618d703.

---
+++ src/AppDev/AdventureWorksTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventureWorksLibrary;
+
+namespace AdventureWorksTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AWLTEntities awlt = new AWLTEntities();
+
+            var products = awlt.Products.Where(p => p.Name.StartsWith("F")).OrderBy(s => s.Name);
+
+            Console.WriteLine("Selected Products:");
+            Console.WriteLine("------------------");
+
+            foreach (var product in products) {
+                Console.WriteLine("{0}, ${1}", product.Name, product.ListPrice.ToString("#,##0.00"));
+            }
+
+            Console.WriteLine("Press <enter> to continue...");
+            Console.ReadLine();
+        }
+    }
+}
